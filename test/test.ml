249a1bcfc178bcda3d96d58c14a657a4e31c4bfe
Patrick Ferris <patrick@sirref.org>
2022-06-19 21:29:32 +0100

    Add a lowercase base32 CID

--- test/test.ml
+++ test/test.ml
@@ -6,7 +6,13 @@ let tests = [
     ~version:`Cidv1
     ~base:`Base58btc
     ~codec:`Raw
-    ~hash:(Md.make ~ident:`Sha2_256 ~length:(256 / 8) ~digest:(Cstruct.of_hex "6E6FF7950A36187A801613426E858DCE686CD7D7E3C0FC42EE0330072D245C95"))
+    ~hash:(Md.make ~ident:`Sha2_256 ~length:(256 / 8) ~digest:(Cstruct.of_hex "6E6FF7950A36187A801613426E858DCE686CD7D7E3C0FC42EE0330072D245C95"));
+  "bafyreihdb57fdysx5h35urvxz64ros7zvywshber7id6t6c6fek37jgyfe",
+  Cid.v
+    ~version:`Cidv1
+    ~base:`Base32
+    ~codec:`Dag_cbor
+    ~hash:(Md.make ~ident:`Sha2_256 ~length:(256 / 8) ~digest:(Cstruct.of_hex "E30F7E51E257E9F7DA46B7CFB9174BF9AE2D238491FA07E9F85E2915BFA4D829"))
 ]
 
 let cid = Alcotest.testable Cid.pp_human Cid.equal
